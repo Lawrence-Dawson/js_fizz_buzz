ee7d40cbf3b7504e1633f60d5818c7ca93616c58
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-09-12 15:52:27 +0100

    first test fr number % 3 passes

---
+++ jasmine-standalone-2.5.0/spec/fizzBuzzSpec.js
@@ -0,0 +1,15 @@
+describe('FizzBuzz', function() {
+
+  var fizzBuzz;
+
+  beforeEach(function() {
+    fizzBuzz = new FizzBuzz();
+  });
+
+
+describe("Fizzbuzz", function() {
+  it("returns fizz on numbers divisible by 3", function() {
+    expect(fizzBuzz.play(3)).toEqual("Fizz");
+  });
+});
+});
