dfcfc87c03098c8f2f52fd001511c1598dc49114
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-09-12 16:35:37 +0100

    FizzBuzz in JS works!!!

--- jasmine-standalone-2.5.0/spec/fizzBuzzSpec.js
+++ jasmine-standalone-2.5.0/spec/fizzBuzzSpec.js
@@ -7,9 +7,21 @@ describe('FizzBuzz', function() {
   });
 
 
-describe("Fizzbuzz", function() {
-  it("returns fizz on numbers divisible by 3", function() {
-    expect(fizzBuzz.play(3)).toEqual("Fizz");
+  describe("Fizzbuzz", function() {
+    it("returns fizz on numbers divisible by 3", function() {
+      expect(fizzBuzz.play(3)).toEqual("Fizz");
+    });
+
+    it("returns buzz on numbers divisible by 5", function() {
+      expect(fizzBuzz.play(5)).toEqual("Buzz");
+    });
+
+    it("returns fizzbuzz on numbers divisible by 3 and 5", function() {
+      expect(fizzBuzz.play(15)).toEqual("FizzBuzz");
+    });
+
+    it("returns the number if no Fizz, buzz, fizzbuzz applies", function() {
+      expect(fizzBuzz.play(2)).toEqual(2);
+    })
   });
-});
 });
--- jasmine-standalone-2.5.0/src/fizzBuzz.js
+++ jasmine-standalone-2.5.0/src/fizzBuzz.js
@@ -2,7 +2,22 @@ function FizzBuzz() {
 }
 
 FizzBuzz.prototype.play = function(number) {
+  if (number % 3 === 0 && number % 5 ===0) {
+    return 'FizzBuzz';
+  }
+
   if (number % 3 === 0) {
     return 'Fizz';
   }
+
+  if (number % 5 === 0) {
+    return 'Buzz';
+  }
+
+  else{
+    return number;
+  }
+
+
+
 }
